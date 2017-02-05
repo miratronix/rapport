67ed4c3511b08bd222ed7a62a5f18faf56e50c23
Lukas Rezek <lukas@miratronix.com>
2017-02-04 22:48:04 -0500

    Add tests for standardize

--- test/index.js
+++ test/index.js
@@ -3,3 +3,78 @@
 const chai = require('chai');
 chai.use(require('chai-as-promised'));
 chai.should();
+
+const TestUtil = {
+
+    mockNodeWebsocket: () => {
+        const ws = {
+            messagesSent: 0,
+            closed: false,
+            handlers: {
+                message: [],
+                close: [],
+                error: [],
+                open: []
+            },
+
+            fire: (type, ...data) => {
+                for (let i = 0; i < ws.handlers[type].length; i++) {
+                    ws.handlers[type][i](...data);
+                }
+            },
+
+            send: () => {
+                ws.messagesSent++;
+            },
+
+            close: () => {
+                ws.closed = true;
+            },
+
+            on: (type, handler) => {
+                ws.handlers[type].push(handler);
+            },
+
+            removeAllListeners: (type) => {
+                ws.handlers[type] = [];
+            }
+        };
+
+        return ws;
+    },
+
+    mockBrowserWebsocket: () => {
+        const ws = {
+            messagesSent: 0,
+            closed: false,
+            onmessage: null,
+            onopen: null,
+            onerror: null,
+            onclose: null,
+
+            fire: (type, ...data) => {
+                if (type === 'message') {
+                    ws.onmessage(...data);
+                } else if (type === 'open') {
+                    ws.onopen(...data);
+                } else if (type === 'close') {
+                    ws.onclose(...data);
+                } else {
+                    ws.onerror(...data);
+                }
+            },
+
+            send: () => {
+                ws.messagesSent++;
+            },
+
+            close: () => {
+                ws.closed = true;
+            }
+        };
+
+        return ws;
+    }
+};
+
+module.exports = TestUtil;
